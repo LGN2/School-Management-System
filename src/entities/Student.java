d0c7d5e456dabb22e1d4978cc1781b0a73e85a4d
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-17 14:58:52 +0400

    Added constructor in Student

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -7,4 +7,19 @@ public class Student {
     private String enrollmentDate;
     private double feeBalance;
     private List<String> subjects;
+
+    public Student(String id
+            ,String firstName
+            ,String lastName
+            ,String dateOfBirth
+            ,String gender
+            ,String phoneNumber
+            ,String email
+            ,String address
+            ,String gradeLevel
+            ,String enrollmentDate
+            ,double feeBalance
+    ) {
+
+    }
 }
