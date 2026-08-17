f30df5849bb1b6fe47de44520121c6649faba283
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-17 16:48:01 +0400

    feat(utils): Teacher consturctor

--- src/entities/Teacher.java
+++ src/entities/Teacher.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Teacher extends Person{
@@ -7,4 +8,32 @@ public class Teacher extends Person{
     private int experienceYears;
     private double salary;
     private List<String> classesTaught;
+
+    public Teacher(String id
+            ,String firstName
+            ,String lastName
+            ,String dateOfBirth
+            ,String gender
+            ,String phoneNumber
+            ,String email
+            ,String address
+            ,String subject
+            ,int experienceYears
+            ,double salary
+    ) {
+        super(id
+                ,firstName
+                , lastName
+                , dateOfBirth
+                , gender
+                , phoneNumber
+                , email
+                , address);
+        this.subject = subject;
+        setExperienveYears(experienceYears);
+        this.salary = salary;
+        this.classesTaught = new ArrayList<>();
+    }
+
+
 }
