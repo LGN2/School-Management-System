375d7f84f4ba96fb324d5be8f269191f09c9559b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-17 11:29:32 +0400

    Added attributes to person in entities

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,4 +1,14 @@
 package entities;
 
-public class Person {
+import interfaces.Displayable;
+
+public class Person implements Displayable {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
 }
