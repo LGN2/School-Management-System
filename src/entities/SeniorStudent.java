8a26df494d3a244d8a6eb072dc2798809cfd93e6
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-18 15:05:30 +0400

    feat(utils): SeniorStudent constructor

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -3,4 +3,32 @@ package entities;
 public class SeniorStudent extends Student{
     private String major;
     private double gpa;
+
+    public SeniorStudent(String id,
+                         String firstName,
+                         String lastName,
+                         String dateOfBirth,
+                         String gender,
+                         String phoneNumber,
+                         String email,
+                         String address,
+                         String gradeLevel,
+                         String enrollmentDate,
+                         double feeBalance,
+                         String major,
+                         double gpa) {
+        super(id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                gradeLevel,
+                enrollmentDate,
+                feeBalance);
+        this.major = major;
+        this.gpa = gpa;
+    }
 }
