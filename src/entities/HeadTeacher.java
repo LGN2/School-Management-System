5ce3d9f565e31b435b1a345438a49688bd397253
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-18 15:11:45 +0400

    feat(utils): HeadTeacher constructor

--- src/entities/HeadTeacher.java
+++ src/entities/HeadTeacher.java
@@ -4,5 +4,31 @@ public class HeadTeacher extends Teacher{
     private String department;
     private int teachersManaged;
 
-
+    public HeadTeacher(String id,
+                       String firstName,
+                       String lastName,
+                       String dateOfBirth,
+                       String gender,
+                       String phoneNumber,
+                       String email,
+                       String address,
+                       String subject,
+                       int experienceYears,
+                       double salary,
+                       String department,
+                       int teachersManaged) {
+        super(id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                subject,
+                experienceYears,
+                salary);
+        this.department = department;
+        this.teachersManaged = teachersManaged;
+    }
 }
